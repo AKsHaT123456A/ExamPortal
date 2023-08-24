762e2704928497af4935f6f4b6dda8b390a06186
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-08-24 16:06:12 +0530

    Update:Category added

--- Controllers/catController.js
+++ Controllers/catController.js
@@ -1,8 +1,13 @@
 const User = require("../Models/user");
 
-const category = (req,res)=>{
-    const {category,id} = req.params;
-    const user = User.findByIdAndUpdate(id, category);
+const category = (req, res) => {
+    try {
+        const { category, id } = req.params;
+        const user = User.findByIdAndUpdate(id, category);
+        return res.status(200).json({ message: "Category updated successfully", category:user.category });
+    } catch (err) {
+        return res.status(500).json({ error: "Internal Server Error", message: err.message });
+    }
 }
 
 module.exports = category;
