dd62097a462b5bb143bfcbdf225c07e4a48a08d6
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-09-14 01:01:17 +0530

    User

--- validators/userValidationSchema.js
+++ validators/userValidationSchema.js
@@ -7,8 +7,8 @@ const userValidationSchema = Joi.object({
   isHosteler: Joi.boolean(),
   isVerified: Joi.boolean(),
   name: Joi.string().regex(/^[a-zA-Z]+(?: [a-zA-Z.]+){0,2}$/).required(),
-  studentNo: Joi.string().pattern(/^[2][12](([x]{3})|[0-9]{2,3})([0-9]){3}(-d)?$/).required(),
-  branch: Joi.string().valid('IT', 'CSE', 'CSE-AIML', 'AIML', 'CS', 'EN', 'ECE', 'ME', 'CSE-DS', 'CSIT', 'CE','CSE-HINDI'),
+  studentNo: Joi.string().pattern(/^22(?:153|31|154|12|164|13|11|169|21|31|00|40)\d{3}$/).required(),
+  branch: Joi.string().valid('IT', 'CSE', 'CSE-AIML', 'AIML', 'CS', 'EN', 'ECE', 'ME', 'CSE-DS', 'CSIT', 'CE', 'CSE-HINDI'),
   responses: Joi.array().items(Joi.string()),
   password: Joi.string().min(6).required(),
   logintime: Joi.number(),
@@ -16,6 +16,6 @@ const userValidationSchema = Joi.object({
   isSubmit: Joi.boolean(),
   category: Joi.string(),
   score: Joi.number(),
-}).prefs({ convert: false }); 
+}).prefs({ convert: false });
 
 module.exports = userValidationSchema;
