1454bca23af024606123d3bb91f9b67b0bea9294
pushkar-28 <kharepushkar2804@gmail.com>
2023-09-17 11:30:09 +0530

    testing to user

--- Controllers/managerialController.js
+++ Controllers/managerialController.js
@@ -1,39 +1,50 @@
 const manage = require("../Models/managerial");
-const { testingUser } = require("../Models/testingUser");
+// const { User } = require("../Models/User");
 const { User } = require("../Models/user");
 // const sendEmail = require("../Utils/manageEmailer");
 // const sendEmail = require("../Utils/manageEmailer");
 
 const managerial = async (req, res) => {
-    const { id } = req.params;
-    try {
-        console.log(id);
-        const user = await User.findById(id);
+  const { id } = req.params;
+  try {
+    console.log(id);
+    const user = await User.findById(id);
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        // Extract user properties
-        const { name, email, studentNo, mobileNo, gender, branch, isVerified, isHosteler } = user;
-
-        const newManagerial = await manage.create({
-            name,
-            email,
-            studentNo,
-            mobileNo,
-            gender,
-            branch,
-            isVerified,
-            isHosteler,
-        });
-        console.log(newManagerial);
-
-        return res.status(201).redirect("https://cine-2023.vercel.app/managerial");
-    } catch (err) {
-        console.error("Error:", err);
-        return res.status(500).json({ error: "Internal Server Error", message: err.message });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    // Extract user properties
+    const {
+      name,
+      email,
+      studentNo,
+      mobileNo,
+      gender,
+      branch,
+      isVerified,
+      isHosteler,
+    } = user;
+
+    const newManagerial = await manage.create({
+      name,
+      email,
+      studentNo,
+      mobileNo,
+      gender,
+      branch,
+      isVerified,
+      isHosteler,
+    });
+    console.log(newManagerial);
+
+    return res.status(201).redirect("https://cine-2023.vercel.app/managerial");
+  } catch (err) {
+    console.error("Error:", err);
+    return res
+      .status(500)
+      .json({ error: "Internal Server Error", message: err.message });
+  }
 };
 
 module.exports = managerial;
