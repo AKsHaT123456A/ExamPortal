66bdebfc1b7fff5fa1faa32f3dc6765ea79aff3c
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2024-11-10 03:56:27 +0530

    Added:Prometheus and Grafana

--- monitoring/requestHistogram.js
+++ monitoring/requestHistogram.js
@@ -2,7 +2,7 @@ import client from "prom-client";
 export const activeRequestHistogram = new client.Histogram({
     name: "http_request_duration_seconds",
     help: "Duration of HTTP requests in ms",
-    labelNames: ["method", "route"],
+    labelNames: ["method", "route","code"],
     buckets: [0.1, 0.5, 1, 1.5, 2, 3, 5, 100, 400, 600],
 });
 
